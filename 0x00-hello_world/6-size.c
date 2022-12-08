860abb941d80b52fe2aec80fbaf1389c6a0d33fb
kencom2019 <ookokennedy1@gmail.com>
2022-12-09 02:01:20 +0300

    origin

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{	pritf("Size of a char: %lu byte(s)\n", sizeof(char));
-	pritf("Size of an int: %lu byte(s)\n", sizeof(int));
-	pritf("Size of a long int %lu byte(s)\n", sizeof(long int));
-	pritf("Size of long long : %lu byte(s)\n", sizeof(long long int));
-	pritf("Size of a float: %lu byte(s)\n", sizeof(float))
+{	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int %lu byte(s)\n", sizeof(long int));
+	printf("Size of long long : %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float))
 	return (0);
 }
