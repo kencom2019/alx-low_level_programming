27c2c4db608af02983114ff09025414d78a4c6f8
kencom2019 <ookokennedy1@gmail.com>
2022-12-11 17:36:54 +0300

    print all strings from 0 to  10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints all single
+ * digit numbers of base 10 starting from 0
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+
+	printf("\n");
+
+	return (0);
+}
