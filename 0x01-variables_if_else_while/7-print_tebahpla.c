f90fc9208dbc259e91b6d5f2607281e871eff380
kencom2019 <ookokennedy1@gmail.com>
2022-12-11 18:31:14 +0300

    alphabet lower case in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main -  prints lower case in reverse
+ * Return: Always 0
+ */
+int main(void)
+{
+	char le;
+
+	for (le = 'z"; le > = 'a'; le==)
+		putchar(le);
+
+	putchar('\n');
+
+	return (0);
+}
