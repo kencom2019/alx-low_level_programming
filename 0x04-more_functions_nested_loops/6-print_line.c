683ea796851f46df43460a6a8d2239779882d11a
kencom2019 <ookokennedy1@gmail.com>
2022-12-15 21:30:29 +0300

    all

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#includec "main.h"
+
+/**
+ * print_line - prints straight line is printed.
+ * @n: no return.
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(95);
+	}
+	_putchar('\n');
+}
