75892d9c006d18cdba5c53b21806f5b571716ffb
kencom2019 <ookokennedy1@gmail.com>
2022-12-16 04:50:30 +0300

    testing c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,18 +1,13 @@
-
 /**
- * _isupper -checks if parameter is an uppercase character.
- * @c: input character.
- * Return: 0 or 1
- */
-int _isupper(int c);
+* _isupper -checks if parameter is an uppercase character.
+* @c: input character.
+* Return: 0 or 1
+*/
+int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
-	{
 		return (1);
-	}
 	else
-	{
 		return (0);
-	}
 }
 
