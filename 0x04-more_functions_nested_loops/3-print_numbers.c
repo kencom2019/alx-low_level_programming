2c5d7aa2724ea34817f8594f13478b10dd293e1f
kencom2019 <ookokennedy1@gmail.com>
2022-12-18 03:43:14 +0300

    printing numbers 0-9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,13 +5,13 @@
  * Return: void
  */
 
-void print_numbers(void);
+void print_numbers(void)
 {
-	char c;
+	int n;
 
-	for (c = '0'; c <= '9'; c++)
-	{
-		_putchar(c);
+	for (n = 0; n <= 9; n++)
+	{_putchar((n % 10) + '0');
 	}
-	_putchar ('\n');
+	_putchar('\n');
+
 }
