4b9f656544987bef9b470c4e4337b42fe744389e
kencom2019 <ookokennedy1@gmail.com>
2022-12-18 04:01:03 +0300

    origin

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,12 +7,14 @@
  */
 void print_most_numbers(void)
 {
-	char c;
+	int n;
 
-	for (c '0'; c <= '9'; c++)
+	for (n = 0; n <= 9; n++)
 	{
-		if (!(c == '2' || c == '4'))
-			_putchar(c);
+	if (n != 2 && n != 4)
+	{
+	_putchar((n % 10) + '0');
+	}
 	}
 	_putchar('\n');
 }
