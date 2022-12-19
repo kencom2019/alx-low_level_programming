691d026729848236c113a1483e7bf32844834842
kencom2019 <ookokennedy1@gmail.com>
2022-12-19 17:00:10 +0300

    my first pointersarray

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,23 +8,26 @@
 void print_triangle(int size);
 
 {
-	int x, y, z;
+	int hash, index;
 
-	if (size <= 0)
+	if (size > 0)
 	{
+	for (hash = 1; hash <= size; hash++)
+	{
+	for (index = size - hash; index > 0; index--)
+	{
+	_putchar(' ');
+	}
+	for (index = 0; index < hash; index++)
+	{
+	_putchar('#');
+	}
+	if (hash == size)
+	{
+	continue;
+	}
 	_putchar('\n');
 	}
-	else
-	{
-	for (x = 0; x < size; x++)
-	}
-	for (y = size - x; y > 1; y--)
-	{
-	_putchar(32);
-	}
-	for (z = 0; z <= x; z++)
-	{
-	_putchar(35);
 	}
 	_putchar('\n');
 }
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+0x05C_Pointers_arrays_and_strings
