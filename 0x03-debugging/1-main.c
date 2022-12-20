c8ac8af62505f9ca7df86736f8dd206e7b079919
kencom2019 <ookokennedy1@gmail.com>
2022-12-21 01:45:00 +0300

    task1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -4,6 +4,7 @@
  * main - causes an infinite loop
  * Return: 0
  */
+
 int main(void)
 {
 	int i;
@@ -17,6 +18,5 @@ int main(void)
 	 /*putchar(i);*/
 	 /*}*/
 	printf("Infinite loop avoided! \\o/\n");
-
 	return (0);
 }
