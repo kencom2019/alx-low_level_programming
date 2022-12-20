257445305609d571b456f478a1f2cbd18eab2f12
kencom2019 <ookokennedy1@gmail.com>
2022-12-20 12:24:28 +0300

    al

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -15,7 +15,7 @@ int _atoi(char *s)
 	temp = s;
 	num = 0;
 	sign = 1;
-	while (temp != '\0' && (*temp < '0' || *temp > '9'))
+	while (*temp != '\0' && (*temp < '0' || *temp > '9'))
 	{
 		if (*temp == '-')
 			sign *= -1;
