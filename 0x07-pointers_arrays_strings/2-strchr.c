bcd6d9f57b28dbfe9a82d7b5eca529aaa07ce401
kencom2019 <ookokennedy1@gmail.com>
2023-01-06 17:55:12 +0300

    all

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: string
+ * @c: character to search
+ * Return: pointer to the first occurence of the character c
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0') /*Declaring WHILE*/
+	{
+		if (*s == c) /*if s == c*/
+		{
+			return (s); /*return s*/
+		}
+		++s;
+	}
+	if (*s == c)
+	{
+		return (s);
+	}
+	return (0);/* values null*/
+}
