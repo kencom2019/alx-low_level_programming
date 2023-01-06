6515eccc1bdb60fec3913515ebfd45ba0e9177ec
kencom2019 <ookokennedy1@gmail.com>
2023-01-06 17:31:34 +0300

    all

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * _memcpy - fills memory with another buffer.
- * @dest: source string
- * @src: string for filling 
- * @n: length of buffer
- * Return: new string
- */
+*_memcpy - fills memory with another buffer
+*@src: string for filling
+*@n:length of buffer
+*@dest: source string
+*Return: new string
+*/
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i = 0;
