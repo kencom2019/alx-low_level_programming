6a88cc389c832618df3b6b22002ac79d9d8c491b
kencom2019 <ookokennedy1@gmail.com>
2023-02-02 14:35:11 +0300

    all

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,9 +14,10 @@
 char *create_array(unsigned int size, char c)
 {
 	char *array = malloc(size)
+
 	if (size == 0 || array == 0)
 		return (NULL);
-	while (size ==)
+	while (size == )
 		array[size] = c;
 	return (array);
 }
